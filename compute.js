48a4ae3138a60fd0dd3fc53adfd6eb8208c9dab7
Andrei Lucaci <lucaciandrei@outlook.com>
2020-02-21 12:53:59 +0200

    alternate ordering - not so socring at all

--- compute.js
+++ compute.js
@@ -5,21 +5,6 @@ const compute = fileName => {
   const input = readFile(fileName)
   const { libraries } = input
 
-  const orderedLibraries = [...libraries]
-    .map(x => {
-      x.potentialScore = x.bookIds
-        .map(x => input.bookScores[x])
-        .reduce((c, d) => c + d)
-      return x
-    })
-    .sort((a, b) => {
-      const firstSortCriteria = a.T - b.T
-      const secondSortCriteria = b.M - a.M
-      const thirdSortCriteria = b.potentialScore - a.potentialScore
-
-      return firstSortCriteria || secondSortCriteria || thirdSortCriteria
-    })
-
   const createOrderedbookDic = arr => {
     const dic = {}
 
@@ -32,6 +17,19 @@ const compute = fileName => {
 
     return dic
   }
+  const books = createOrderedbookDic(input.bookScores)
+
+  let orderedLibraries = []
+  const computeOrderedLibraries = () => {
+    orderedLibraries = [...libraries].sort((a, b) => {
+      const firstSortCriteria = a.T - b.T
+      const secondSortCriteria = b.M - a.M
+
+      return firstSortCriteria || secondSortCriteria
+    })
+  }
+
+  computeOrderedLibraries()
 
   const result = {
     libraries: {},
@@ -46,11 +44,62 @@ const compute = fileName => {
     return rLib
   }
 
-  const books = createOrderedbookDic(input.bookScores)
-  const signedUpLibs = []
+  let signedUpLibs = []
   let sigLib = orderedLibraries.shift()
 
   const processedBooks = {}
+
+  const recomputeOrderedLibraries = tMax => {
+    if (orderedLibraries.length) {
+      orderedLibraries = [
+        ...(orderedLibraries.length ? orderedLibraries : libraries),
+      ]
+        .filter(x => x.bookIds.length && tMax > x.T)
+        .sort((a, b) => {
+          // const firstSortCriteria = a.T - b.T
+          // const secondSortCriteria = b.M - a.M
+
+          const bookScores = x => {
+            const vb = x.bookIds
+              .filter(y => !processedBooks.hasOwnProperty(y))
+              .map(y => books[y])
+              .sort((c, d) => d.val - c.val)
+              .slice(0, (tMax - x.T) * x.M)
+
+            const score = vb.length
+              ? vb.reduce((acc, vl) => acc + Number(vl.val), 0)
+              : 100
+
+            return score
+          }
+
+          const bScore = bookScores(b)
+          const aScore = bookScores(a)
+          b.bookScore = bScore
+          a.bookScore = aScore
+
+          return bScore - aScore
+
+          // return firstSortCriteria || secondSortCriteria
+
+          // const cs = x => (tMax - x.T) * x.M
+          // const css = (x, csx) => {
+          //   const itms = x.bookIds.slice(0, csx).filter(y => y)
+          //   if (itms.length) {
+          //     itms.reduce((c, d) => {
+          //       const res = c + books[d].val
+          //       return res
+          //     })
+          //   }
+          // }
+          // const [csb, csa] = [cs(b), cs(a)]
+          // if (csb > 0 && csa > 0) {
+          //   const [cssb, cssa] = [css(b, csb), css(a, csa)]
+          //   return cssb - cssa
+          // }
+        })
+    }
+  }
 
   for (let i = 0; i < input.D; i++) {
     if (!(i % 1000)) {
@@ -58,7 +107,10 @@ const compute = fileName => {
     }
 
     if (signedUpLibs.length) {
-      for (let sLib of signedUpLibs) {
+      signedUpLibs = signedUpLibs.filter(x => x.bookIds.length)
+
+      for (let j = 0; j < signedUpLibs.length; j++) {
+        const sLib = signedUpLibs[j]
         let rLib = result.libraries[sLib.id]
         if (!rLib) {
           rLib = createLib(sLib)
@@ -68,21 +120,31 @@ const compute = fileName => {
           .map(x => books[x])
           .sort((a, b) => b.val - a.val)
 
-        let j = 0
-        while (j < sLib.M && unprocessedBooks.length) {
+        let k = 0
+        while (k < sLib.M && unprocessedBooks.length) {
           const ub = unprocessedBooks.shift()
           const bkId = ub.i
 
-          const index = sLib.bookIds.indexOf(bkId)
-          if (index > -1) {
-            sLib.bookIds.splice(index, 1)
+          for (let l = j; l < signedUpLibs.length; l++) {
+            const lLib = signedUpLibs[l]
+            const index = lLib.bookIds.indexOf(bkId)
+            if (index > -1) {
+              lLib.bookIds.splice(index, 1)
+            }
           }
 
           if (processedBooks[bkId]) continue
 
+          orderedLibraries.forEach(x => {
+            const index = x.bookIds.indexOf(bkId)
+            if (index > -1) {
+              x.bookIds.splice(index, 1)
+            }
+          })
+
           processedBooks[bkId] = true
           rLib.books.push(bkId)
-          j++
+          k++
         }
       }
     }
@@ -91,7 +153,14 @@ const compute = fileName => {
       sigLib.T--
       if (!sigLib.T) {
         signedUpLibs.push(sigLib)
+        recomputeOrderedLibraries(input.D - i)
+
         sigLib = orderedLibraries.shift()
+        if (sigLib) {
+          console.log(
+            `Now processing lib[${sigLib.id}] with a score of: ${sigLib.bookScore}`
+          )
+        }
       }
     }
   }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const compute = require('./compute')
 
 const inputFiles = [
-  'a_example.txt',
-  'b_read_on.txt',
-  'c_incunabula.txt',
-  'd_tough_choices.txt',
+  // 'a_example.txt',
+  // 'b_read_on.txt',
+  // 'c_incunabula.txt',
+  // 'd_tough_choices.txt',
   'e_so_many_books.txt',
   'f_libraries_of_the_world.txt',
 ]
